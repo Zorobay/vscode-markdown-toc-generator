ea4402cfd8697e143e1de3014d63bc912efdd2be
Sebastian Hegardt <=>
2019-07-09 15:28:29 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,31 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { window, ExtensionContext, commands, TextDocument, Position, TextEditorEdit } from 'vscode';
+import { Markdown } from './markdown';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "markdown-toc-generator" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+
+	let command = commands.registerCommand('extension.generateTOC', () => {
+		let editor = window.activeTextEditor;
+		if (editor) {
+			let doc = editor.document;
+			let markdown = new Markdown(editor, doc);
+			markdown.createOrUpdateTOC();
+		}
+	});
+
+	context.subscriptions.push(command);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
---
+++ src/markdown.ts
@@ -0,0 +1,208 @@
+import { window, Position, TextDocument, TextEditorEdit, Range, TextLine, InputBoxOptions, TextEditor, DocumentHighlight, TextEdit } from "vscode";
+import { range, some } from 'lodash';
+import { build } from "xregexp";
+
+// Define constants to mark start and end of markdown TOC
+const TOC_START_COMMENT = "[comment]: # (---START_OF_TOC---)";
+const TOC_END_COMMENT = "\n[comment]: # (---END_OF_TOC---)\n";
+
+// Define constant for tab size
+const TAB = "   ";
+
+// Define regexps
+const REG_HEADING = new RegExp(String.raw`\s{0,3}#{2,5}\s*[\.\w]+`, 'i');
+//const REG_HASH = new RegExp(String.raw`#{1,5}`, 'i');
+//const REG_TITLE = new RegExp(String.raw`^#{1}\s*[a-zA-Z]+`, 'i');
+const REG_HEADING_PARTS = new RegExp(String.raw`(\s*#*)\s([0-9.]*)\s*(.*)`, 'i');
+//const REG_TOC = new RegExp(String.raw`^\s*[\*-]\s*[0-9.]*\s*\[[\w\s\-\(\)\/,]+\]\(#`, 'i');
+//const REG_TOC_TITLE = new RegExp(String.raw`#\s+`, 'i');
+const REG_SPACES = new RegExp(String.raw`\s+`, 'ig');
+const REG_DOTS = new RegExp(String.raw`\.`, 'g');
+//const REG_DATE_OF_TITLE = new RegExp(String.raw`\([0-9\-]+\)`, 'i');
+
+
+export class Markdown {
+
+    editor: TextEditor;
+    document: TextDocument;
+
+    constructor(editor: TextEditor, document: TextDocument) {
+        this.editor = editor;
+        this.document = document;
+    }
+
+    createOrUpdateTOC() {
+        let res = this.tocPresent();
+        if (res.toc_start > 0) {
+            window.showInformationMessage("Updating existing Table of Contents!");
+            this.updateTOC(res);
+        } else if (res.context > 0) {
+            window.showInformationMessage("Creating new Table of Contents!");
+            this.insertNewTOC(res.context);
+        }
+    }
+
+    /**
+     * Checks if an existing TOC exists. Returns an object with line number of start of TOC (or -1 if non-existent),
+     * end of TOC (or -1 if non-existent) and the line number where the user opened the context menu.
+     */
+    tocPresent(): { toc_start: number, toc_end: number, context: number } {
+        let contextLine: number = -1;
+        let tocStartLine: number = -1;
+        let tocEndLine: number = -1;
+
+
+        // Find position of context menu
+        contextLine = this.editor.selection.active.line;
+
+        // Walk up to find top comment
+        let hasTopComment: boolean = false;
+        for (var i = contextLine; i >= 0; i--) {
+            if (this.document.lineAt(i).text.includes(TOC_START_COMMENT.trim())) {
+                hasTopComment = true;
+                tocStartLine = i;
+                break;
+            }
+        }
+
+        // Walk down to find bottom comment if top comment existed
+        let hasBottomComment: boolean = false;
+        if (hasTopComment) {
+            for (i = contextLine; i <= this.document.lineCount; i++) {
+                if (this.document.lineAt(i).text.includes(TOC_END_COMMENT.trim())) {
+                    hasBottomComment = true;
+                    tocEndLine = i;
+                    break;
+                }
+            }
+        }
+
+        if (!(hasTopComment && hasBottomComment)) {
+            tocStartLine = -1;
+        }
+
+        return { toc_start: tocStartLine, toc_end: tocEndLine, context: contextLine };
+    }
+
+    async updateTOC(locs: { toc_start: number, toc_end: number, context: number }) {
+        // Remove old TOC
+
+        if (locs.toc_start > 0 && locs.toc_end > 0 && locs.toc_start < locs.toc_end) {
+            await this.replaceTOC(locs.toc_start, locs.toc_end);
+        }
+    }
+
+    async removeTOC(begin: number, end: number) {
+        if (this.document.lineAt(begin).text.includes(TOC_START_COMMENT)) {
+            await this.editor.edit((editBuilder: TextEditorEdit) => {
+                editBuilder.delete(new Range(new Position(begin, 0), new Position(end + 1, 0)));
+            });
+        }
+    }
+
+    async replaceTOC(toc_start: number, toc_end: number) {
+        let toc = new TableOfContents(this.document);
+        let range: Range = new Range(new Position(toc_start, 0), new Position(toc_start + toc.getNumberOfLines()-1, 0));
+
+        await this.editor.edit((editBuilder: TextEditorEdit) => {
+            editBuilder.replace(range, toc.toString());
+        });
+    }
+
+    async insertNewTOC(toc_start: number) {
+        let toc = new TableOfContents(this.document);
+
+        await this.editor.edit((editBuilder: TextEditorEdit) => {
+            editBuilder.insert(new Position(toc_start, 0), toc.toString());
+        });
+    }
+
+    static getBullet(level: number) {
+        return (level === 2) ? '* ' : ((level > 2) ? '- ' : '');
+    }
+}
+
+class TableOfContents {
+
+    document: TextDocument;
+    tocLines: string[] = [];
+
+    constructor(document: TextDocument) {
+        this.document = document;
+
+        this.buildTOC();
+    }
+
+    buildTOC() {
+        let tocLines: string[] = [TOC_START_COMMENT];
+        let allLines: string[] = this.document.getText().split("\n");
+
+        for (let line of allLines) {
+            if (line.match(REG_HEADING)) {
+                let header = new Header(line);
+                let level = header.getLevel();
+                let chapter = header.getChapter();
+                let text = header.getText();
+                let anchor = header.getAnchor();
+                let bullet = Markdown.getBullet(level);
+                tocLines.push(`${TAB.repeat(level - 2)}${bullet}${chapter} [${text.trim()}](#${anchor})`);
+            }
+        }
+
+        tocLines.push(TOC_END_COMMENT);
+        this.tocLines = tocLines;
+    }
+
+    toString() {
+        return this.tocLines.join("\n");
+    }
+
+    getNumberOfLines() {
+        let lines:number = 0;
+        for(let line of this.tocLines) {
+            lines++;
+            let newlines = line.match(/\n/g);
+            if (newlines) {
+                lines = lines + newlines.length;
+            }
+        }
+        return lines;
+    }
+
+}
+
+class Header {
+
+    header: string;
+    level: number = -1;
+    chapter: string = '?';
+    text: string = '?';
+
+    constructor(header: string) {
+        this.header = header;
+        let parts = REG_HEADING_PARTS.exec(this.header);
+        if (parts) {
+            this.level = parts[1].length;
+            this.chapter = parts[2];
+            this.text = parts[3];
+        }
+    }
+
+    getLevel(): number {
+        return this.level;
+    }
+
+    getChapter(): string {
+        return this.chapter;
+    }
+
+    getText(): string {
+        return this.text;
+    }
+
+    getAnchor(): string {
+        let anchorBase: string = `${this.chapter} ${this.text}`;
+        return anchorBase.replace(REG_DOTS, '').replace(REG_SPACES, '-').toLowerCase();
+    }
+}
+
